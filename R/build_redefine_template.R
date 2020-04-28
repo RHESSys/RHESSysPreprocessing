fa9b383159dc766e84ff69d1ce8c856f5238d024
William Burke <burke24@gmail.com>
2020-04-28 01:18:57 -0700

    Small documentation update

--- R/build_redefine_template.R
+++ R/build_redefine_template.R
@@ -1,27 +1,19 @@
 #' Build Redefine Template
 #'
-#' Builds a template file for use in a redefine world.  Allows for specification of which variables to override and replace
-#' with an absolute or multiplier value. Should be paired with world_redefine function (which is essentially world_gen), to
-#' create the actual redefine worldfile.
+#' Builds a template file for use in a redefine world. Pair with world_redefine.
 #' @param template Original template file used to generate your worldfile for RHESSys.
 #' @param name Name of output redefine template to be created
-#' @param replace DOESNT WORK YET -Indicates the variable-method-values to replace. All others will be given values of "-9999"
 #' @param overwrite Overwrite existing redefine template? FALSE is default and prompts a menu if worldfile already exists.
 #' @author Will Burke
 #' @export
 
-build_redefine_template = function(template, name, replace, overwrite) {
+build_redefine_template = function(template, name, overwrite) {
 
   template_list = template_read(template)
 
   template_clean = template_list[[1]] # template in list form
-  #var_names = template_list[[2]] # names of template vars
   level_index = template_list[[3]] # index of level separators in template_clean/var_names
   var_index = template_list[[4]] # index of vars
-  #map_info = template_list[[5]] # tables of maps and their inputs/names in the template
-  #head = template_list[[6]] # header
-  #maps_in = unique(map_info[,2])
-
 
   redef_template = template_clean
 
@@ -29,9 +21,6 @@ build_redefine_template = function(template, name, replace, overwrite) {
   for (i in var_index) {
     redef_template[[i]] = c(redef_template[[i]][1], "value", "-9999")
   }
-
-  # Replace specific values here
-  # NOT HERE YETTT
 
   # print template
   template_write = vector(mode = "character")
