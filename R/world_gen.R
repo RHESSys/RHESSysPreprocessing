a93312ee1a1b447ff6d27c2e0a6ff00dcd2ae7f1
William Burke <burke24@gmail.com>
2023-10-02 09:05:35 -0700

    Update world_gen.R

--- R/world_gen.R
+++ R/world_gen.R
@@ -21,7 +21,9 @@ world_gen = function(template,
                      convert_aspect = TRUE,
                      wrapper = FALSE) {
 
+  # ----------------------------------------------------------------
   # -------------------- Input & Error Checking --------------------
+  # ----------------------------------------------------------------
   options(scipen = 999) # no scientific notation - prevents automatic conversion later
 
   if (!file.exists(template)) {
@@ -43,7 +45,9 @@ world_gen = function(template,
   }
   worldfile = file.path(dirname(worldfile),worldname)
 
+  # ----------------------------------------------------------
   # -------------------- Read in Template --------------------
+  # ----------------------------------------------------------
   template_list = template_read(template)
 
   template_clean = template_list[[1]] # template in list form
@@ -61,7 +65,9 @@ world_gen = function(template,
     }
   }
 
+  # ------------------------------------------------------
   # -------------------- Read in Maps --------------------
+  # ------------------------------------------------------
   read_maps = GIS_read(
     maps_in = maps_in,
     type = type,
@@ -83,19 +89,28 @@ world_gen = function(template,
   cellarea = rep(cellarea, length(map_df[,1]))
   rm(read_maps) # read_maps may be very large if maps are large
 
-  # structure to iterate through spatial levels ---- matrix with unique ID's for each unit at each level
+  # check the maps are there
+  # if (any(!map_info[,"Map"] %in% names(map_df))) {
+  #   stop("Missing map")
+  # }
+
+  # names
   w_map = map_info[map_info[,1] == "world",2]
   b_map = map_info[map_info[,1] == "basin",2]
   h_map = map_info[map_info[, 1] == "hillslope", 2]
   z_map = map_info[map_info[, 1] == "zone", 2]
   p_map = map_info[map_info[, 1] == "patch", 2]
   s_map = map_info[map_info[, 1] == "strata", 2]
-  #levels = unname(data.matrix(map_df[c(w_map,b_map,h_map,z_map,p_map,s_map)], length(map_df[p_map])))
-  # if we run into memory issues, re factor this code
+  levelmaps = unname(c(w_map,b_map,h_map,z_map,p_map,s_map))
 
-  levels = data.matrix(map_df[c(w_map,b_map,h_map,z_map,p_map,s_map)], length(map_df[p_map]))
-  levels = as.data.frame(levels)
+  # sort map df first so everything is subsequently in order
+  map_df = map_df[order(map_df[,h_map], map_df[,z_map], map_df[,p_map], map_df[,s_map], decreasing = F),]
+  rownames(map_df) = NULL
+
+  # structure to iterate through spatial levels ---- matrix with unique ID's for each unit at each level
+  levels = map_df[levelmaps]
   colnames(levels) = c("w", "b", "h", "z", "p", "s")
+  # levels = levels[order(levels$h, levels$z, levels$p, levels$s, decreasing = F),] # always order now, by hill, zone, patch, strata, in that order
 
   # mode for aggregating by mode
   mode_fun = function(x) {
@@ -103,7 +118,9 @@ world_gen = function(template,
     ux[which.max(tabulate(match(x, ux)))]
   }
 
+  # -------------------------------------------------------------------
   # -------------------- Aspatial Patch Processing --------------------
+  # -------------------------------------------------------------------
   rulevars = NULL
   if (asp_check) {
     asp_map = template_clean[[which(var_names == "asp_rule")]][3] # get rule map/value
@@ -114,19 +131,31 @@ world_gen = function(template,
     }
     rulevars = aspatial_patches(asprules = asprules, asp_mapdata = asp_mapdata)
 
+    # TODO  revisit this and directly use the map df maybe
     if (is.data.frame(asp_mapdata)) { # add ruleID to levels df
-      asp_level = aggregate(asp_mapdata[[1]] , by = as.list(levels), FUN = mode_fun)
-      levels = merge(levels, asp_level[,c("h", "z", "p", "x")], by = c("h", "z", "p"), sort = F)
+      asp_level = aggregate(asp_mapdata[[1]] , by = as.list(levels[,c("z","p")]), FUN = mode_fun, simplify = F)
+      levels = merge(x = levels, y = asp_level[,c("z", "p", "x")], by = c("z", "p"), sort = F)
       levels = levels[c("w", "b", "h", "z", "p", "s", "x")]
       names(levels)[7] = "a"
-      #levels = cbind(levels, a = unname(as.matrix(asp_mapdata)))
+
+      # check rules - 1 rule per patch
+      check_rules_per_patch = function(rules, patches) {
+        if (length(rules) != length(patches)) {return("Something failed in rule per patch check - number of rules and patches are different in inputs")}
+        rule_counts = sapply(split(rules,patches), function(X){length(unique(unlist(X)))})
+        if (any(rule_counts > 1)) {stop(paste0("Multiple rules found per patch - correct using mode failed for some reason. Problem patches: ",
+                                               rule_counts[rule_counts > 1]))}
+      }
+      check_rules_per_patch(levels$a, levels$p)
+
     } else if (is.numeric(asp_mapdata)) {
       levels = cbind(levels, a = rep(asp_mapdata,length(levels[,1])) )
     }
     level_index = c(level_index, length(template_clean)+2) # because rules got added to the levels matrix, this is to prevent them from being aggregated across
   }
 
+  # -------------------------------------------------------------------
   # -------------------- Additional Input Checking --------------------
+  # -------------------------------------------------------------------
   # Error checking for basestationID/number - it's a loop :/ but short so not a big deal
   # If n_basestations is 0 -> do NOT include base_station_ID
   # If n_basestations is > 0 -> must include base_station_ID, even if doing redefine w/ -9999
@@ -163,7 +192,9 @@ world_gen = function(template,
     }
   } # end loop through n_basestations
 
+  # -----------------------------------------------------------------
   # -------------------- Process Template + Maps --------------------
+  # -----------------------------------------------------------------
   # Build list based on operations called for by template
   statevars = vector("list",length(template_clean))
 
@@ -254,8 +285,9 @@ world_gen = function(template,
     }
   }
 
+  # ----------------------------------------------------------
   # -------------------- Write World File --------------------
-
+  # ----------------------------------------------------------
   # functions for replacing lapplys
   # basin
   bfun = function(i) {
@@ -294,7 +326,7 @@ world_gen = function(template,
   print("Writing worldfile",quote = FALSE)
   stratum = 1:template_clean[[level_index[6]]][3] # count of stratum
 
-  # ----- Progress Bar -----
+  # -------------------- Progress Bar --------------------
   # Iterates at hillslope level, shouldnt slow code too much
   progress = 0
   pb = txtProgressBar(min = 0, max = 1,style = 3)
@@ -303,14 +335,14 @@ world_gen = function(template,
   # create/open connection
   wcon = file(worldfile,open = "wt")
 
-  # ----- World -----
+  # -------------------- World --------------------
   # No state variables at world level
   world = unique(levels$w)
   writeChar(paste(world,"\t\t\t","world_ID\n",sep = ""),con = wcon, eos = NULL)
   writeChar(paste(length(unique(levels$b)),"\t\t\t","num_basins\n",sep = ""),con = wcon,eos = NULL)
   basin = unique(levels$b)
 
-  # ----- Basin -----
+  # -------------------- Basin --------------------
   for (b in basin) {
     writeChar(paste("\t",b,"\t\t\t","basin_ID\n",sep = ""),con = wcon,eos = NULL)
     bout = unlist(lapply((level_index[2] + 1):(level_index[3] - 1), FUN = bfun))
@@ -319,7 +351,7 @@ world_gen = function(template,
     hillslopes = unique(levels[levels$b == b, "h"])
     writeChar(paste("\t",length(hillslopes),"\t\t\t","num_hillslopes\n",sep = ""),con = wcon,eos = NULL)
 
-    # ----- Hillslope -----
+    # -------------------- Hillslope --------------------
     for (h in hillslopes) {
       # Iterate progress bar
       progress = progress + 1
@@ -332,13 +364,13 @@ world_gen = function(template,
       zones = unique(levels[levels$h == h & levels$b == b, "z"])
       writeChar(paste("\t\t",length(zones),"\t\t\t","num_zones\n",sep = ""),con = wcon,eos = NULL)
 
-      # ----- Zone -----
+      # -------------------- Zone --------------------
       for (z in zones) {
         writeChar(paste("\t\t\t",z,"\t\t\t","zone_ID\n",sep = ""),con = wcon,eos = NULL)
         zout = unlist(lapply((level_index[4] + 1):(level_index[5] - 1), FUN = zfun))
         writeChar(zout,con = wcon,eos = NULL)
 
-        #---------- Start multiscale (aspatial) patches and stratum ----------
+        # -------------------- START MSR - multiscale (aspatial) patches and stratum --------------------
         if (asp_check) {
           patches = unique(levels[levels$z == z & levels$h == h & levels$b == b, "p"])
           asp_ct = sapply(rulevars, FUN = function(x) ncol(x[[1]]) - 1)
@@ -348,25 +380,22 @@ world_gen = function(template,
           } else {
             total_patches = sum(asp_ct[names(asp_ct) %in% unique(levels[levels$z == z & levels$h == h & levels$b == b,])[,"a"]])
           }
-
           writeChar(paste("\t\t\t",total_patches,"\t\t\t","num_patches\n",sep = ""),con = wcon,eos = NULL)
 
-
-          # ----- Patches (spatial) -----
+          # -------------------- MSR Patches (spatial) --------------------
           for (p in patches) {
             ruleid = paste0("rule_", unique(levels[(levels$p == p & levels$z == z & levels$h == h & levels$b == b), "a"]))
 
             if (length(ruleid) != 1) {stop("Multiple rule ids found for patch: ",p)}
             asp_index = 1:(length(rulevars[[(ruleid)]]$patch_level_vars[1,]) - 1)
 
-            # ----- Patches (non-spatial) -----
+            # -------------------- MSR Patches (non-spatial) --------------------
             for (asp in asp_index) {
               pnum = (p*100) + asp # adjust patch numbers here - adds two 0's, ie: patch 1 becomes patches 101, 102, etc.
 
               writeChar(c(paste("\t\t\t\t",pnum,"\t\t\t","patch_ID\n",sep = ""),
                           paste("\t\t\t\t",p,"\t\t\t","family_ID\n",sep = "")),
                         con = wcon,eos = NULL)
-              #writeChar(paste("\t\t\t\t",p,"\t\t\t","family_ID\n",sep = ""),con = wcon,eos = NULL)
 
               asp_p_vars = which(!rulevars[[ruleid]]$patch_level_vars[,1] %in% var_names[var_index]) # get vars from aspatial not included in template
 
@@ -402,7 +431,7 @@ world_gen = function(template,
 
               writeChar(paste("\t\t\t\t",strata_ct,"\t\t\t","num_stratum\n",sep = ""),con = wcon,eos = NULL)
 
-              # ----- Canopy Strata (for non-spatial patches) -----
+              # -------------------- MSR Canopy Strata (for non-spatial patches) --------------------
               for (s in 1:strata_ct) {
 
                 # Adds unique IDs for strataum intialized w/ same map as patches - appends 1 or 2 to the patch ID, ie patch 30 would have stratum 301 and 302
